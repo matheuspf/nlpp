872d85c125ee2aa3b6261bc5cd199cf7652d494f
matheuspf <matheuspedrozaferreira@uft.edu.br>
2018-06-06 08:47:07 -0300

    Fix problem with include

--- include/nlpp/Helpers/Include.h
+++ include/nlpp/Helpers/Include.h
@@ -2,16 +2,16 @@
 
 
 #ifndef EIGEN_INCLUDE_LOCAL
-    #define EIGEN_INCLUDE_LOCAL 0
+    #define EIGEN_INCLUDE_LOCAL 1
 #endif
 
 #define EIGEN_INCLUDE_LOCAL_RELEASE 0
-#define EIGEN_INCLUDE_GLOBAL 1
+#define EIGEN_INCLUDE_GLOBAL 0
 
 
 #if EIGEN_INCLUDE_LOCAL
 
-    #include "Eigen/Eigen/Dense"
+    #include "../Eigen/Eigen/Dense"
 
 #elif EIGEN_INCLUDE_LOCAL_RELEASE
 
@@ -40,6 +40,6 @@
 
 #elif HANDY_INCLUDE_LOCAL
 
-    #include "handy/include/Handy.h"
+    #include "../handy/include/Handy.h"
 
 #endif
