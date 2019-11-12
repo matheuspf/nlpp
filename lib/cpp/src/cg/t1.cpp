aee7d50486d9f4400b701b0d2bc0ba878c6157cc
matheuspf <matheuspedrozaferreira@uft.edu.br>
2019-11-12 19:24:09 -0300

    Add code to test

---
+++ lib/cpp/src/cg/t1.cpp
@@ -0,0 +1,19 @@
+#include "lib/cpp/include/cg/cg.hpp"
+#include "TestFunctions/Rosenbrock.h"
+
+
+int main ()
+{
+    nlpp_p::CG<> opt;
+    // Opt<CGType, LS, Stop, Out> opt(LS{}, 1e4);
+
+    nlpp::Rosenbrock func;
+    nlpp::Vec x0 = nlpp::Vec::Constant(10, 2.0);
+
+    auto res = opt(func, x0);
+
+    handy::print(res.transpose());
+
+
+    return 0;
+}
