a8c8608e130886a6dcaa71177e8995879f62072c
matheuspf <matheuspedrozaferreira@uft.edu.br>
2020-09-10 06:52:12 -0400

    Update CmakeLists; Update submodules

--- cmake/get_project_version.cmake
+++ cmake/get_project_version.cmake
@@ -3,13 +3,13 @@ function(getProjectVersion config_file_path)
     ### Get major.minor.patch semantic version
     file(READ ${config_file_path} config_file)
 
-    string(REGEX MATCH "define[ \t]+${lib_name_upper}_MAJOR_VERSION[ \t]+([0-9]+)" lib_name_major_version_match ${config_file})
+    string(REGEX MATCH "define[ \t]+NLPP_MAJOR_VERSION[ \t]+([0-9]+)" lib_name_major_version_match ${config_file})
     set(lib_name_major_version "${CMAKE_MATCH_1}")
 
-    string(REGEX MATCH "define[ \t]+${lib_name_upper}_MINOR_VERSION[ \t]+([0-9]+)" lib_name_minor_version_match ${config_file})
+    string(REGEX MATCH "define[ \t]+NLPP_MINOR_VERSION[ \t]+([0-9]+)" lib_name_minor_version_match ${config_file})
     set(lib_name_minor_version "${CMAKE_MATCH_1}")
 
-    string(REGEX MATCH "define[ \t]+${lib_name_upper}_PATCH_VERSION[ \t]+([0-9]+)" lib_name_patch_version_match ${config_file})
+    string(REGEX MATCH "define[ \t]+NLPP_PATCH_VERSION[ \t]+([0-9]+)" lib_name_patch_version_match ${config_file})
     set(lib_name_patch_version "${CMAKE_MATCH_1}")
 
     set(lib_full_version ${lib_name_major_version}.${lib_name_minor_version}.${lib_name_patch_version} PARENT_SCOPE)
